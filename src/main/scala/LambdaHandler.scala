f8ffdf2a9f6c26a86d0a5d0ddb0ef8e3a6e98e22
itwasmycode <kemalemrecolak@gmail.com>
2023-08-03 01:38:31 +0300

    testing purpose 10

--- src/main/scala/LambdaHandler.scala
+++ src/main/scala/LambdaHandler.scala
@@ -3,8 +3,17 @@ import com.amazonaws.lambda.thirdparty.com.google.gson.GsonBuilder
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
 
-object LambdaHandler {
-  def lambdaHandler(args: Array[String]) = {
-    println("Hello, world")
+class LambdaHandler() extends RequestHandler[JavaMap[String, String], String] {
+  val gson = new GsonBuilder().setPrettyPrinting().create
+
+  override def handleRequest(event: JavaMap[String, String], context: Context): String = {
+    val logger = context.getLogger
+
+    logger.log(s"ENVIRONMENT VARIABLES: ${gson.toJson(System.getenv)}\n")
+    logger.log(s"CONTEXT: ${gson.toJson(context)}\n")
+
+    logger.log(s"EVENT: ${gson.toJson(event)}\n")
+
+    "Hello from Scala!"
   }
 }
